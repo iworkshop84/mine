1f1d4bf2788e7547b2e754c6445e38966929a0d2
iworkshop <iworkshop84@gmail.com>
2017-11-01 20:57:12 +0200

    + Сделал страницу серверлист у пользователя
    Signed-off-by: iworkshop <iworkshop84@gmail.com>

--- views/users/serverlist.php
+++ views/users/serverlist.php
@@ -54,16 +54,58 @@
 
     <div id="posts">
 
-        <div id="serverheader">
-            <div id="shcount">#</div>
-            <div id="shulname">Название</div>
-            <div id="shuladress">Адрес</div>
-            <div id="shulstatus">Статус</div>
-            <div id="shulplayers">Игроков</div>
-            <div id="shulversion">Версия</div>
-            <div id="shulreith">Рейтинг</div>
+
+        <div class="serverpretext">
+            <h2>
+                Список серверов пользователя: <?= $_SESSION['login'];?>
+
+            </h2>
+        </div>
+        <div class="serverprehead">
+
+            <div class="serverlist_headposition">
+                <i class="icon-signal"></i>
+            </div>
+            <div class="serverlist_headname">
+                <i class="icon-name"></i>
+                <span>Название сервера</span>
+            </div>
+            <div class="serverlist_ip">
+                <i class="icon-serverlist_ip"></i>
+                <span> IP Адрес</span>
+            </div>
+            <div class="serverlist_status">
+                <i class="icon-serverlist_status"></i>
+                <span> Статус</span>
+            </div>
+
+            <div class="server_headplayers">
+                <i class="icon-players"></i>
+                <span> Игроков</span>
+            </div>
+            <div class="server_headversion">
+                <i class="icon-version"></i>
+                <span> Версия</span>
+            </div>
+            <div class="serverlist_headvote">
+                <i class="icon-vote"></i>
+                <span> Голосов</span>
+            </div>
+            <div class="server_headuptime">
+                <i class="icon-uptime"></i>
+                <span> Аптайм</span>
+            </div>
 
         </div>
+
+
+
+
+
+
+
+
+
         <div class="clear"></div>
         <div id="servers">
             <?php foreach ($items as $key=>$item): ?>
@@ -73,19 +115,21 @@
                         <a href="<?= '/servers/one/' . $item->id ?>"><?= substr($item->name, 0, 30) . '...' ?></a>
                     </div>
                     <div class="sulipport">
-
-                        <span class="ip"><?= $item->ip . ':' . $item->port ?></span>
+                        <div class="server_status">
+                            <div class="server_adres_text"><?= $item->ip . ':' . $item->port ?></div>
+                        </div>
                     </div>
                     <div class="sulstatus">
                         <?php if($item->online == 0): ?>
-                            <span class="offline">Офлайн</span>
+                            <span class="offline">Offline</span>
                         <?php else: ?>
-                            <span class="online">Онлайн</span>
+                            <span class="online">Online</span>
                         <?php endif; ?>
                     </div>
                     <div class="sulplayers"><?= $item->players . ' из ' . $item->maxplayers ?></div>
                     <div class="sulversion"><?= $item->version ?></div>
                     <div class="sulvotes"><?= $item->votes ?></div>
+                    <div class="suluptime"><?= $item->uptime .'%'; ?></div>
 
                     <div class="suledit"><a href="<?= '/users/sedit/' . $item->id ?>">Редактировать</a></div>
                     <div class="suldelete"><a href="<?= '/users/sdelete/' . $item->id ?>">Удалить</a></div>
@@ -94,25 +138,27 @@
         </div>
         <div class="clear"></div>
 
-        <div class="oservere">
-            <span>Подтвердить права на сервер</span>
-        </div>
-        <div id="serverapruvtext">
-            <p>
-                Для того чтобы вы могли менять описание сервера он должен быть закреплён за вами в нашем рейтинге.
-                При добавлении сервера зарегистрированным пользователем он автоматически прикрепляется к нему и больше ничего делать не нужно.
-            </p>
-            <p>
-                Если же вы добавили сервер будучи незарегистрированным пользователем или же кто-то добавил ваш сервер до вас -
-                вы в любой момент можете подтвердить то, что сервер принадлежит вам и перенести его в свой аккаунт в нашем рейтинге.
-            </p>
-            <p>
-                Для этого вам нужно изменить название своего сервера так, чтобы оно заказнчивалось на <b><?= 'uid'. $_SESSION['uid']; ?></b>.
-                Чаще всего, чтобы новое название вступило в силу необходимо перезапустить сервер Майнкрафт.
-                После этого добавьте в поле ниже адрес своего сервера в формате <b>ip:порт</b>(например 127.0.0.1:25565) и нажмите подтвердить.
-            </p>
-            <p>После того, как сервер успешно пройдет проверку, его название можно сменить на первоначальное.</p>
+        <div id="serverlistcontainer">
+            <div class="oservere">
+                <span>Подтвердить права на сервер</span>
+            </div>
+            <div id="serverapruvtext">
+                <p>
+                    Для того чтобы вы могли менять описание сервера он должен быть закреплён за вами в нашем рейтинге.
+                    При добавлении сервера зарегистрированным пользователем он автоматически прикрепляется к нему и больше ничего делать не нужно.
+                </p>
+                <p>
+                    Если же вы добавили сервер будучи незарегистрированным пользователем или же кто-то добавил ваш сервер до вас -
+                    вы в любой момент можете подтвердить то, что сервер принадлежит вам и перенести его в свой аккаунт в нашем рейтинге.
+                </p>
+                <p>
+                    Для этого вам нужно изменить название своего сервера так, чтобы оно заказнчивалось на <b><?= 'uid'. $_SESSION['uid']; ?></b>.
+                    Чаще всего, чтобы новое название вступило в силу необходимо перезапустить сервер Майнкрафт.
+                    После этого добавьте в поле ниже адрес своего сервера в формате <b>ip:порт</b>(например 127.0.0.1:25565) и нажмите подтвердить.
+                </p>
+                <p>После того, как сервер успешно пройдет проверку, его название можно сменить на первоначальное.</p>
 
+            </div>
         </div>
         <div class="clear"></div>
 
